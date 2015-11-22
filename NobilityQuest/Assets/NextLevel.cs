22b6a4dcb6da37912db6a7e2f24de68437446e9f
Cantankerous55 <ramassini_john@cl.fcasd.edu>
2015-11-22 15:19:00 -0500

    Initial Commit of Project

---
+++ NobilityQuest/Assets/NextLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	public int LevelNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		Application.LoadLevel (LevelNumber);
+	}
+}
---
+++ NobilityQuest/Assets/Scripts/WillmotAttack2.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof (Animator))]
+public class WillmotAttack2 : MonoBehaviour 
+{
+	private Animator m_animator;
+	private Animator m_droidDeath;
+	private bool canKill;
+	public Transform lineStart, lineEnd;
+	private bool isAnimating;
+	private Animation death;
+	public Particle deathanim;
+	private bool isDying;
+	public float deathWaitTime;
+	public GameObject DeathAnim;
+	//bool hasStartedDeath = false;
+
+	RaycastHit2D whatIHit;
+
+	void Start()
+	{
+		m_animator = GetComponent<Animator> ();
+		isDying = false;
+		DeathAnim = GameObject.Find ("Death Anim") ;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Raycasting ();
+	}
+
+
+	void Raycasting ()
+	{
+		Debug.DrawLine (lineStart.position, lineEnd.position, Color.red);
+
+		if (Physics2D.Linecast (lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer("Enemy") ) ) 
+	    {
+			whatIHit = (Physics2D.Linecast (lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer("Enemy"))); 
+			canKill = true;
+			Debug.Log ("I can Kill " + whatIHit.transform.name);
+		}
+		else 
+		{
+			canKill = false;
+			Debug.Log ("Can't kill anything.");
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			m_animator.Play ("WillmotAttack");
+			if (canKill) {
+				Destroy (whatIHit.collider.gameObject);
+				//m_droidDeath = whatIHit.collider.gameObject.GetComponent<Animator> ();
+				//m_droidDeath.Play ("droidDeath");
+				//isDying = true;
+				//m_droidDeath.SetBool ("isDying", isDying);
+
+				//Instantiate ( DeathAnim, new Vector3 (whatIHit.collider.gameObject.transform.position.x, whatIHit.transform.position.y, whatIHit.transform.position.z), (whatIHit.collider.gameObject.transform.rotation));
+
+
+
+
+				/*if (hasStartedDeath = false) 
+				{
+					//whatIHit.collider.gameObject.GetComponent<Animator>().SetTrigger("DeathAnimation");
+					//StartCoroutine(DeathWait(whatIHit.collider.gameObject));
+				}
+
+			} else {
+				if (canKill) {
+					Destroy (whatIHit.collider.gameObject);
+				}
+
+			}
+		}
+	}
+		IEnumerator DeathWait (GameObject objectToDestroy) 
+		{
+			hasStartedDeath = true;
+			yield return new WaitForSeconds (1f);
+			Destroy (objectToDestroy.gameObject);
+			StopCoroutine ("DeathWait");
+		}*/
+}
+
+		}
+
+	}
+}
